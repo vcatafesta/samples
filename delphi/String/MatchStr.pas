b1375aa1f073d7d5dd1d9b3785249cd5f88562f7
Vilmar Catafesta <vcatafesta@gmail.com>
2021-06-30 13:43:38 -0400

    Create MatchStr.pas

---
+++ delphi/String/MatchStr.pas
@@ -0,0 +1,11 @@
+Procedure TForm1.txtMesExit(Sender: TObject);
+Var
+  Txt: String;
+Begin
+  Txt := TxtMes.Text;
+  If Not MatchStr(txt, aMeses) Then
+  Begin
+    ShowMessage('Mês inválido.');
+    TxtMes.SetFocus;
+  End;
+End;
