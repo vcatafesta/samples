2c8dc3648317b723d7716846f3da74b1f9bfac63
Vilmar Catafesta <vcatafesta@gmail.com>
2021-07-13 02:23:23 -0400

    Update CreateComponentAtRuntime.pas

--- delphi/CreateComponentAtRuntime.pas
+++ delphi/CreateComponentAtRuntime.pas
@@ -4,13 +4,15 @@ Begin
   MyEdit := TEdit.Create(Self);
   With MyEdit Do
   Begin
-    Top    := 48;
-    Left   := 296;
-    Width  := 200;
-    Height := 21;
-    Text   := 'Teste ';
-    Color  := clYellow;
-    Parent := Form1;
+    Left       := 32;
+    Top        := 64;
+    Width      := 209;
+    Height     := 21;
+    Text       := 'EDIT CRIADO VIA RUNTIME';
+    Color      := clYellow;
+    Font.Name  := 'Segoe UI';
+    Font.Size  := 10;
+    Font.Style := [fsBold];
+    Parent     := grp2;
   End;
   Application.ProcessMessages;
-End;
