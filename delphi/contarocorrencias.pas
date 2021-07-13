cea47d5f915071a55bd6833a7c1705e98500a6bc
Vilmar Catafesta <vcatafesta@gmail.com>
2021-07-13 02:26:50 -0400

    Update contarocorrencias.pas

--- delphi/contarocorrencias.pas
+++ delphi/contarocorrencias.pas
@@ -24,3 +24,41 @@ Begin
       Result := Result + 1;
   End;
 End;
+
+Function Ocorrencias(TextoProcurado, Texto: String): integer;
+Var
+  posicao: integer;
+Begin
+  result  := 0;
+  posicao := PosEx(TextoProcurado, Texto, 1);
+  While posicao > 0 Do
+  Begin
+    inc(result);
+    posicao := PosEx(TextoProcurado, Texto, posicao + length(TextoProcurado));
+  End;
+End;
+
+Function QtdeCaracter(cLetra, Texto: String): Integer;
+Var
+  i: Integer;
+Begin
+  Result := 0;
+  Texto  := Trim(Texto);
+  For i  := 1 To Length(Texto) Do
+  Begin
+    If Texto[i] = cLetra Then
+      Result := Result + 1;
+  End;
+End;
+
+Function CountInText(Find, Text: String): Integer;
+Begin
+  Result := 0;
+  While (Pos(Find, Text) > 0) Do
+  Begin
+    Text := Copy(Text, 1, Pos(Find, Text) - 1) + Copy(Text, Pos(Find, Text) + Length(Find), Length(Text));
+    Inc(Result, 1);
+  End;
+End;
+
+
